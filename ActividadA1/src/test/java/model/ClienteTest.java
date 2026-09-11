7642e5d166e7feaafff249822170d07464081e0f
manuelxt07 <manuels.roserom@uqvirtual.edu.co>
2026-09-11 16:19:12 -0500

    Al agregar el parametro faltante a Biciclta, en el test tambien se necesitaba ese parametro para completar su constructor

--- ActividadA1/src/test/java/model/ClienteTest.java
+++ ActividadA1/src/test/java/model/ClienteTest.java
@@ -16,8 +16,8 @@ class ClienteTest {
 
         Taller taller = new Taller("Taller");
         Cliente cliente = new Cliente("Manuel", taller, "1085915590", "3185454373", "Calle 4 N 4 - 16", new ArrayList<>());
-        Bicicleta bici1 = new Bicicleta("Trek", "Rojo", "SOR1", 2022);
-        Bicicleta bici2 = new Bicicleta("Giant", "Negro", "SEP2", 2023);
+        Bicicleta bici1 = new Bicicleta("Trek", "Rojo", "SOR1", 2022,TipoBicicleta.ELECTRICA, taller);
+        Bicicleta bici2 = new Bicicleta("Giant", "Negro", "SEP2", 2023, TipoBicicleta.MTB, taller);
 
         cliente.agregarBicicleta(bici1);
         cliente.agregarBicicleta(bici2);
