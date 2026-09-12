ed1f4d12f61435d87d91944b65a915929cdc4630
manuelxt07 <manuels.roserom@uqvirtual.edu.co>
2026-09-12 11:39:26 -0500

    .

--- ActividadA1/src/main/java/module-info.java
+++ ActividadA1/src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module BiciTaller {
     requires javafx.controls;
     requires javafx.fxml;
-
-    opens app to javafx.fxml;
-    opens controller to javafx.fxml;
-
+    opens viewController to javafx.fxml;
     exports app;
 }
